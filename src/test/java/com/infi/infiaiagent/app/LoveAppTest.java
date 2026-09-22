ba4e3b838ba6b807e5328c3e9a7cf3869d423c68
qq_32523587 <qq_32523587@noreply.gitcode.com>
2026-09-22 16:36:06 +0800

    feat(chat): 添加基于文件的对话记忆功能和恋爱咨询应用
    - 实现了 FileBasedChatMemory 类，支持对话历史的文件持久化存储
    - 集成 Kryo 序列化库用于高效的消息数据存储

---
+++ src/test/java/com/infi/infiaiagent/app/LoveAppTest.java
@@ -0,0 +1,46 @@
+package com.infi.infiaiagent.app;
+
+import cn.hutool.core.lang.UUID;
+import jakarta.annotation.Resource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class LoveAppTest {
+
+    @Resource
+    private LoveApp loveApp;
+
+    @Test
+    void testChat() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员infi";
+        String answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+//        // 第二轮
+//        message = "我想让另一半（编程导航）更爱我";
+//        answer = loveApp.doChat(message, chatId);
+//        Assertions.assertNotNull(answer);
+//        // 第三轮
+//        message = "我的另一半叫什么来着？刚跟你说过，帮我回忆一下";
+//        answer = loveApp.doChat(message, chatId);
+//        Assertions.assertNotNull(answer);
+        // 第三轮
+        message = "我叫什么来着？刚跟你说过，帮我回忆一下";
+        answer = loveApp.doChat(message, chatId);
+        Assertions.assertNotNull(answer);
+    }
+
+    @Test
+    void doChatWithReport() {
+        String chatId = UUID.randomUUID().toString();
+        // 第一轮
+        String message = "你好，我是程序员infi，我想让另一半（编程导航）更爱我，但我不知道该怎么做";
+        LoveApp.LoveReport loveReport = loveApp.doChatWithReport(message, chatId);
+        Assertions.assertNotNull(loveReport);
+    }
+}
